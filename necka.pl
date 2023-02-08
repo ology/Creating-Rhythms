1223deb98bf2476855e4c3726dea82d049e59b27
Gene Boggs <gene@ology.net>
2023-02-07 22:17:11 -0800

    Initial commit

---
+++ necka.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/env perl
+
+# Generate binary necklaces of length n with allowed parts pi.
+
+use strict;
+use warnings;
+
+use Data::Dumper::Compact qw(ddc);
+
+my $n = shift;
+my @aparts = @ARGV; # allowed parts
+
+die "Usage: perl $0 n p1 p2 ...\n"
+  unless $n && @aparts;
+
+my @intervals;
+my @parts = (1);
+my $nap = @aparts;
+my $i = 0;
+
+neckbin(1, 1, 1);
+
+print ddc(\@intervals),
+  'Size: ', scalar @intervals, "\n";
+
+sub allowed {
+  my ($p) = @_;
+
+  for my $i (0 .. $nap - 1) {
+    return 1 if $p == $aparts[$i];
+  }
+
+  return 0;
+}
+
+# k = length of necklace
+# l = length of longest prefix that is a lyndon word
+# m = number of parts (ones)
+# p = size of the next part
+
+sub neckbin {
+    my ($k, $l, $p) = @_;
+
+  if ($k > $n) {
+    if (($n % $l) == 0 && allowed($p) && $p <= $n) {
+      for $k (1 .. $n) {
+        push $intervals[$i]->@*, $parts[$k];
+      }
+
+      $i++;
+    }
+  }
+  else {
+    $parts[$k] = $parts[ $k - $l ];
+    if ($parts[$k] == 1) {
+      if (allowed($p) || $k == 1) {
+        neckbin($k + 1, $l, 1);
+      }
+      $parts[$k] = 0;
+      neckbin($k + 1, $k, $p + 1);
+    }
+    else {
+      neckbin($k + 1, $l, $p + 1);
+    }
+  }
+}
