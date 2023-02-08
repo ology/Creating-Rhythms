318f248279db50e9337a3bb1db9c2a9f2a5bff3d
Gene Boggs <gene@ology.net>
2023-02-08 12:11:27 -0800

    Use Integer::Partition instead of a recursive algorithm

--- part.pl
+++ part.pl
@@ -6,39 +6,16 @@ use strict;
 use warnings;
 
 use Data::Dumper::Compact qw(ddc);
+use Integer::Partition ();
 
 my $n = shift || die "Usage: perl $0 n\n";
 
 my @data;
-my @parts;
-my $i = 0;
 
-partition($n - 1, 1, 0);
+my $i = Integer::Partition->new($n, { lexicographic => 1 });
+while (my $p = $i->next) {
+    push @data, [ sort { $a <=> $b } @$p ];
+}
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
-
-sub partition {
-  my ($n, $p, $m) = @_;
-
-  if ($n == 0) {
-    while ($n < $m) {
-      push $data[$i]->@*, $parts[$n];
-
-      $n++;
-    }
-
-    push $data[$i]->@*, $p;
-
-    $i++;
-
-    return;
-  }
-
-  return if $n < 0;
-
-  $parts[$m] = $p;
-
-  partition($n - $p, $p, $m + 1);
-  partition($n - 1, $p + 1, $m);
-}
