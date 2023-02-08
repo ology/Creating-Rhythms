71efa539ad1f6cb09165711796cf6968364cf587
Gene Boggs <gene@ology.net>
2023-02-08 12:36:39 -0800

    Use Integer::Partition + Data::Munge + List::Util instead of recursing

--- parta.pl
+++ parta.pl
@@ -6,6 +6,9 @@ use strict;
 use warnings;
 
 use Data::Dumper::Compact qw(ddc);
+use Data::Munge qw(list2re);
+use Integer::Partition ();
+use List::Util qw(all);
 
 my $n = shift;
 my @aparts = @ARGV; # allowed parts
@@ -14,51 +17,14 @@ die "Usage: perl $0 n p1 p2 ...\n"
   unless $n && @aparts;
 
 my @data;
-my @parts;
-my $nap = @aparts;
-my $i = 0;
 
-partition($n - 1, 1, 0);
+my $re = list2re @aparts;
+
+my $i = Integer::Partition->new($n, { lexicographic => 1 });
+while (my $p = $i->next) {
+  push @data, [ sort { $a <=> $b } @$p ]
+    if all { $_ =~ /^$re$/ } @$p;
+}
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
-
-sub allowed {
-  my ($p) = @_;
-
-  for my $i (0 .. $nap - 1) {
-    return 1 if $p == $aparts[$i];
-  }
-
-  return 0;
-}
-
-sub partition {
-  my ($n, $p, $m) = @_;
-
-  if ($n == 0) {
-    if (allowed($p)) {
-      while ($n < $m) {
-        push $data[$i]->@*, $parts[$n];
-
-        $n++;
-      }
-
-      push $data[$i]->@*, $p;
-
-      $i++;
-    }
-
-    return;
-  }
-
-  return if $n < 0;
-
-  if (allowed($p)) {
-    $parts[$m] = $p;
-
-    partition($n - $p, $p, $m + 1);
-  }
-
-  partition($n - 1, $p + 1, $m);
-}
