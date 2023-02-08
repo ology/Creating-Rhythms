59c1a046028877bac5eaef5b8d6dab3bffa53409
Gene Boggs <gene@ology.net>
2023-02-08 14:58:13 -0800

    Don't recurse

--- compa.pl
+++ compa.pl
@@ -5,7 +5,11 @@
 use strict;
 use warnings;
 
+use Algorithm::Combinatorics qw(permutations);
 use Data::Dumper::Compact qw(ddc);
+use Data::Munge qw(list2re);
+use Integer::Partition ();
+use List::Util qw(all);
 
 my $n = shift;
 my @aparts = @ARGV; # allowed parts
@@ -14,48 +18,27 @@ die "Usage: perl $0 n p1 p2 ...\n"
   unless $n && @aparts;
 
 my @data;
-my @parts;
-my $nap = @aparts;
-my $i = 0;
+my %seen;
 
-compose($n - 1, 1, 0);
+my $re = list2re @aparts;
+
+my $i = Integer::Partition->new($n, { lexicographic => 1 });
+
+while (my $p = $i->next) {
+  my $iter = permutations($p);
+
+  while (my $x = $iter->next) {
+    next if $seen{"@$x"}++; # skip duplicate permutations
+
+    push @data, $x
+      if all { $_ =~ /^$re$/ } @$x;
+  }
+}
+
+# make sure the data is in lexicographical order
+@data = map { [ split //, $_ ] }
+  sort { $a cmp $b }
+    map { join '', @$_ } @data;
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
-
-sub allowed {
-  my ($p) = @_;
-
-  for my $i (0 .. $nap - 1) {
-    return 1 if $p == $aparts[$i];
-  }
-
-  return 0;
-}
-
-sub compose {
-  my ($n, $p, $m) = @_;
-
-  if ($n == 0) {
-    if (allowed($p)) {
-      while ($n < $m) {
-        push $data[$i]->@*, $parts[$n];
-        $n++;
-      }
-
-      push $data[$i]->@*, $p;
-
-      $i++;
-    }
-
-    return;
-  }
-
-  if (allowed($p)) {
-    $parts[$m] = $p;
-
-    compose($n - 1, 1, $m + 1);
-  }
-
-  compose($n - 1, $p + 1, $m);
-}
