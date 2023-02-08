75974131d9047bfdea440f0b09b44cb392c38f57
Gene Boggs <gene@ology.net>
2023-02-08 10:09:24 -0800

    Initial commit

---
+++ permi.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+# Generate permutations of the given strings.
+
+use strict;
+use warnings;
+
+use Data::Dumper::Compact qw(ddc);
+use Algorithm::Combinatorics qw(permutations);
+
+my @aparts = @ARGV;
+
+die "Usage: perl $0 a1 a2 ... an\n"
+  unless @aparts;
+
+my @data = permutations(\@aparts);
+
+print ddc(\@data),
+  'Size: ', scalar @data, "\n";
