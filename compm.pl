eb8ee771ecc9a9bba5820d910d941fdf0e78ee6c
Gene Boggs <gene@ology.net>
2023-02-08 14:41:14 -0800

    Don't recurse

--- compm.pl
+++ compm.pl
@@ -5,46 +5,33 @@
 use strict;
 use warnings;
 
+use Algorithm::Combinatorics qw(permutations);
 use Data::Dumper::Compact qw(ddc);
+use Integer::Partition ();
 
-my $n  = shift;
-my $mp = shift || die "Usage: perl $0 n m\n";
-
-$mp--;
+my $n = shift;
+my $m = shift || die "Usage: perl $0 n m\n";
 
 my @data;
-my @parts;
-my $i = 0;
+my %seen;
 
-compose($n - 1, 1, 0);
+my $i = Integer::Partition->new($n);
+
+while (my $p = $i->next) {
+    my $iter = permutations($p);
+
+    while (my $x = $iter->next) {
+      next if $seen{"@$x"}++; # skip the duplicate permutations
+
+      push @data, $x
+        if @$x == $m;
+    }
+}
+
+# make sure the data is in lexicographical order
+@data = map { [ split //, $_ ] }
+  sort { $a cmp $b }
+    map { join '', @$_ } @data;
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
-
-sub compose {
-  my ($n, $p, $m) = @_;
-
-  if ($n == 0) {
-    if ($m == $mp) {
-      while ($n < $m) {
-        push $data[$i]->@*, $parts[$n];
-
-        $n++;
-      }
-
-      push $data[$i]->@*, $p;
-
-      $i++;
-    }
-
-    return;
-  }
-
-  if ($m < $mp) {
-    $parts[$m] = $p;
-
-    compose($n - 1, 1, $m + 1);
-  }
-
-  compose($n - 1, $p + 1, $m);
-}
