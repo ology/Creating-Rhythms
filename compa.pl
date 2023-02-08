9e2a42ec5769561398763c90828eb7535c689603
Gene Boggs <gene@ology.net>
2023-02-07 21:42:39 -0800

    Initial commit

---
+++ compa.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+
+# Generate compositions of n with allowed parts pi.
+
+use strict;
+use warnings;
+
+use Data::Dumper::Compact qw(ddc);
+
+my $n = shift;
+my @aparts = @ARGV; # allowed parts
+
+die "Usage: perl $0 n p1 p2 ...\n"
+  unless $n && @aparts;
+
+my @intervals;
+my @parts;
+my $nap = @aparts;
+my $i = 0;
+
+compose($n - 1, 1, 0);
+
+print ddc(\@intervals),
+  'Size: ', scalar @intervals, "\n";
+
+sub allowed {
+  my ($p) = @_;
+
+  for my $i (0 .. $nap - 1) {
+    return 1 if $p == $aparts[$i];
+  }
+
+  return 0;
+}
+
+sub compose {
+  my ($n, $p, $m) = @_;
+
+  if ($n == 0) {
+    if (allowed($p)) {
+      while ($n < $m) {
+        push $intervals[$i]->@*, $parts[$n];
+        $n++;
+      }
+
+      push $intervals[$i]->@*, $p;
+
+      $i++;
+    }
+
+    return;
+  }
+
+  if (allowed($p)) {
+    $parts[$m] = $p;
+
+    compose($n - 1, 1, $m + 1);
+  }
+
+  compose($n - 1, $p + 1, $m);
+}
