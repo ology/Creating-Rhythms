2e69f157bd7bb0202a880052b0e24ed4269fd1b5
Gene Boggs <gene@ology.net>
2023-02-08 18:29:36 -0800

    Add the lex_iter function

--- lib/Util.pm
+++ lib/Util.pm
@@ -43,4 +43,23 @@ sub has_rotation {
   return $found;
 }
 
+=head2 lex_iter
+
+Add the binary numbers from C<0> to C<2 ** N - 1> to a list.
+
+=cut
+
+sub lex_iter {
+  my ($n) = @_;
+
+  my @bin;
+
+  for my $i (0 .. 2 ** $n - 1) {
+    my $x = sprintf '%0*b', $n, $i; 
+    push @bin, $x;
+  }
+
+  return @bin;
+}
+
 1;
