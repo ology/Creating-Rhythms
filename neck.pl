4c017cc8999574227a951ded4453aa7c4300faac
Gene Boggs <gene@ology.net>
2023-02-09 11:50:50 -0800

    Rollback to the good ol' days

--- neck.pl
+++ neck.pl
@@ -5,20 +5,44 @@
 use strict;
 use warnings;
 
-use lib 'lib';
-use Util;
 use Data::Dumper::Compact qw(ddc);
 
 my $n = shift || die "Usage: perl $0 n\n";
 
 my @data;
+my @parts = (1);
+my $i = 0;
 
-my @lex = Util::lex_iter($n);
-
-for my $i (reverse @lex) {
-  push @data, $i
-    unless Util::has_rotation($i, @data);
-}
+neckbin(1, 1);
 
 print ddc(\@data),
-  'Size: ', scalar(@data), "\n";
+  'Size: ', scalar @data, "\n";
+
+# k = length of necklace
+# l = length of longest prefix that is a lyndon word
+
+sub neckbin {
+  my ($k, $l) = @_;
+
+  if ($k > $n) {
+    if(($n % $l) == 0) {
+      for $k (1 .. $n) {
+        push $data[$i]->@*, $parts[$k];
+      }
+
+      $i++;
+    }
+  }
+  else {
+    $parts[$k] = $parts[ $k - $l ];
+
+    if ($parts[$k] == 1) {
+      neckbin($k + 1, $l);
+      $parts[$k] = 0;
+      neckbin($k + 1, $k);
+    }
+    else {
+      neckbin($k + 1, $l);
+    }
+  }
+}
