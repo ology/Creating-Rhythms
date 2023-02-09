ed11d49c5f59499623a0d1e05a48ec9fc7134dfe
Gene Boggs <gene@ology.net>
2023-02-08 17:22:21 -0800

    Do not recurse

--- neckm.pl
+++ neckm.pl
@@ -5,45 +5,26 @@
 use strict;
 use warnings;
 
+use lib 'lib';
+use Util;
 use Data::Dumper::Compact qw(ddc);
 
-my $n  = shift;
-my $n1 = shift || die "Usage: perl $0 n m\n";
+my $n = shift;
+my $m = shift // die "Usage: perl $0 n m\n";
 
+my @lex;
 my @data;
-my @parts = (1);
-my $i = 0;
 
-neckbin(1, 1, 0);
+for my $i (0 .. 2 ** $n - 1) {
+  my $x = sprintf '%0*b', $n, $i; 
+  push @lex, $x;
+}
+
+for my $i (reverse @lex) {
+  my $ones = $i =~ tr/1//;
+  push @data, $i
+    if $ones == $m && !Util::has_rotation($i, @data);
+}
 
 print ddc(\@data),
-  'Size: ', scalar @data, "\n";
-
-# k = length of necklace
-# l = length of longest prefix that is a lyndon word
-# m = number of parts (ones)
-
-sub neckbin {
-  my ($k, $l, $m) = @_;
-
-  if ($k > $n) {
-    if (($n % $l) == 0 && $m == $n1) {
-      for $k (1 .. $n) {
-        push $data[$i]->@*, $parts[$k];
-      }
-
-      $i++;
-    }
-  }
-  else {
-    $parts[$k] = $parts[ $k - $l ];
-    if ($parts[$k] == 1) {
-      neckbin($k + 1, $l, $m + 1);
-      $parts[$k] = 0;
-      neckbin($k + 1, $k, $m);
-    }
-    else {
-      neckbin($k + 1, $l, $m);
-    }
-  }
-}
+  'Size: ', scalar(@data), "\n";
