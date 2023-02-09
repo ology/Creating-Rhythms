6ea8f2017402402ec1b0f1ee89259f3ab040b1d4
Gene Boggs <gene@ology.net>
2023-02-08 17:13:24 -0800

    Do rotation detection

--- lib/Util.pm
+++ lib/Util.pm
@@ -3,6 +3,8 @@ package Util;
 use strict;
 use warnings;
 
+use String::CyclicRotation qw(is_rotation);
+
 =head1 FUNCTIONS
 
 =head2 lex_sort
@@ -16,6 +18,29 @@ sub lex_sort {
   return map { [ split //, $_ ] }
     sort { $a cmp $b }
       map { join '', @$_ } @data;
+}
+
+=head2
+
+Does the given string have a rotation in the list of strings?
+
+=cut
+
+sub has_rotation {
+  my ($string, @strings) = @_;
+
+  my $found = 0;
+
+  for my $i (@strings) {
+    next if $string eq $i;
+
+    if (is_rotation($string, $i)) {
+      $found++;
+      last;
+    }
+  }
+
+  return $found;
 }
 
 1;
