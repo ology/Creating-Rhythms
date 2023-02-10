ccd263378c094ae8191342225d97ddb1c0b5f73a
Gene Boggs <gene@ology.net>
2023-02-09 18:09:36 -0800

    Replace allowed() (for now)

--- neckam.pl
+++ neckam.pl
@@ -1,12 +1,10 @@
 #!/usr/bin/env perl
 
-# Generate binary necklaces of length n with m ones and allowed intervals pi.
+# Generate binary necklaces of length n with m ones and allowed parts pi.
 
 use strict;
 use warnings;
 
-use lib 'lib';
-use Util;
 use Data::Dumper::Compact qw(ddc);
 
 my $n  = shift;
@@ -18,12 +16,23 @@ die "Usage: perl $0 n m p1 p2 ...\n"
 
 my @data;
 my @parts = (1);
+my $nap = @aparts;
 my $i = 0;
 
 neckbin(1, 1, 0, 1);
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
+
+sub allowed {
+  my ($p) = @_;
+
+  for my $i (0 .. $nap - 1) {
+    return 1 if $p == $aparts[$i];
+  }
+
+  return 0;
+}
 
 # k = length of necklace
 # l = length of longest prefix that is a lyndon word
@@ -34,7 +43,7 @@ sub neckbin {
   my ($k, $l, $m, $p) = @_;
 
   if ($k > $n) {
-    if(($n % $l) == 0 && Util::allowed($p, \@aparts) && $p <= $n && $m == $n1) {
+    if(($n % $l) == 0 && allowed($p) && $p <= $n && $m == $n1) {
       for $k (1 .. $n) {
         push $data[$i]->@*, $parts[$k];
       }
@@ -45,7 +54,7 @@ sub neckbin {
   else {
     $parts[$k] = $parts[ $k - $l ];
     if ($parts[$k] == 1) {
-      if (Util::allowed($p, \@aparts) || $k == 1) {
+      if (allowed($p) || $k == 1) {
         neckbin($k + 1, $l, $m + 1, 1);
       }
       $parts[$k] = 0;
