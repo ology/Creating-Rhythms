6b575d1b84aebc4df9721a69bd8e429a9f29b6be
Gene Boggs <gene@ology.net>
2023-02-09 17:57:49 -0800

    Rollback to recursion

--- compm.pl
+++ compm.pl
@@ -5,33 +5,46 @@
 use strict;
 use warnings;
 
-use lib 'lib';
-use Util;
-use Algorithm::Combinatorics qw(permutations);
 use Data::Dumper::Compact qw(ddc);
-use Integer::Partition ();
 
-my $n = shift;
-my $m = shift // die "Usage: perl $0 n m\n";
+my $n  = shift;
+my $mp = shift || die "Usage: perl $0 n m\n";
+
+$mp--;
 
 my @data;
-my %seen;
+my @parts;
+my $i = 0;
 
-my $i = Integer::Partition->new($n);
-
-while (my $p = $i->next) {
-    my $j = permutations($p);
-
-    while (my $q = $j->next) {
-      next if $seen{"@$q"}++; # skip duplicate permutations
-
-      push @data, $q
-        if @$q == $m;
-    }
-}
-
-# make sure the data is in lexicographical order
-@data = Util::lex_sort(@data);
+compose($n - 1, 1, 0);
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
+
+sub compose {
+  my ($n, $p, $m) = @_;
+
+  if ($n == 0) {
+    if ($m == $mp) {
+      while ($n < $m) {
+        push $data[$i]->@*, $parts[$n];
+
+        $n++;
+      }
+
+      push $data[$i]->@*, $p;
+
+      $i++;
+    }
+
+    return;
+  }
+
+  if ($m < $mp) {
+    $parts[$m] = $p;
+
+    compose($n - 1, 1, $m + 1);
+  }
+
+  compose($n - 1, $p + 1, $m);
+}
