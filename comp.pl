64ec5618ca8ec7a55ee27c4591537e70db9d8491
Gene Boggs <gene@ology.net>
2023-02-09 17:54:59 -0800

    Rollback to recursion

--- comp.pl
+++ comp.pl
@@ -5,31 +5,38 @@
 use strict;
 use warnings;
 
-use lib 'lib';
-use Util;
-use Algorithm::Combinatorics qw(permutations);
 use Data::Dumper::Compact qw(ddc);
-use Integer::Partition ();
 
 my $n = shift || die "Usage: perl $0 n\n";
 
 my @data;
-my %seen;
+my @parts;
+my $i = 0;
 
-my $i = Integer::Partition->new($n);
-
-while (my $p = $i->next) {
-    my $j = permutations($p);
-
-    while (my $q = $j->next) {
-      next if $seen{"@$q"}++; # skip the duplicate permutations
-
-      push @data, $q;
-    }
-}
-
-# make sure the data is in lexicographical order
-@data = Util::lex_sort(@data);
+compose($n - 1, 1, 0);
 
 print ddc(\@data),
   'Size: ', scalar @data, "\n";
+
+sub compose {
+  my ($n, $p, $m) = @_;
+
+  if ($n == 0) {
+    while ($n < $m) {
+      push $data[$i]->@*, $parts[$n];
+
+      $n++;
+    }
+
+    push $data[$i]->@*, $p;
+
+    $i++;
+
+    return;
+  }
+
+  $parts[$m] = $p;
+
+  compose($n - 1, 1, $m + 1);
+  compose($n - 1, $p + 1, $m);
+}
