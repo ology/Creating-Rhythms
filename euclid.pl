0436c4ab4587c5b4286533011cd22aee76054b15
Gene Boggs <gene@ology.net>
2023-02-17 21:02:58 -0800

    Clean up

--- euclid.pl
+++ euclid.pl
@@ -5,15 +5,12 @@
 use strict;
 use warnings;
 
-use Data::Dumper::Compact qw(ddc);
-
 my $n = shift;
 my $m = shift || die "Usage: perl $0 n m\n";
 
 my $data = euclid($n, $m);
 
-print join(' ', @$data), "\n";#,
-#  'Size: ', scalar @$data, "\n";
+print join(' ', @$data), "\n";
 
 sub euclid {
     my ($p, $q) = @_;
@@ -25,7 +22,7 @@ sub euclid {
     my @onsets = ('0') x $q;
 
     for my $y ( 1 .. $p ) {
-        $onsets[ sprintf "%.0f", ( $y - $intercept ) / $slope ] = '1';
+        $onsets[ sprintf '%.0f', ( $y - $intercept ) / $slope ] = '1';
     }
 
     return \@onsets;
